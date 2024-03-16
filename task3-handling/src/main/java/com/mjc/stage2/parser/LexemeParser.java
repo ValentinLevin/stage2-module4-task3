34b5de98df2ad8ce6c256be4baa53a6ddfc6f380
Valentin Levin <levin_valentin@mail.ru>
2024-03-16 12:53:32 +0500

    task3

--- task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/LexemeParser.java
@@ -1,9 +1,23 @@
 package com.mjc.stage2.parser;
 
-public class LexemeParser {
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.TextComponent;
+import com.mjc.stage2.entity.TextComponentType;
+
+public class LexemeParser extends AbstractTextParser {
     private static final String LEXEME_REGEX = "\\s+";
     private static final String WORD_REGEX = "\\w[\\w!=?():]+";
 
-    // Write your code here!
-
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        for (String lexeme : string.split(LEXEME_REGEX)) {
+            if (lexeme.matches(WORD_REGEX)) {
+                TextComponent textComponent = new TextComponent(TextComponentType.WORD);
+                abstractTextComponent.add(textComponent);
+                if (nextParser != null) {
+                    nextParser.parse(textComponent, lexeme);
+                }
+            }
+        }
+    }
 }
--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -1,8 +1,15 @@
 package com.mjc.stage2.parser;
 
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.SymbolLeaf;
 
-public class WordParser {
-
-    // Write your code here!
-
+public class WordParser extends AbstractTextParser {
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        int stringLength = string.length();
+        for (int i = 0; i < stringLength; i++) {
+            SymbolLeaf symbolLeaf = new SymbolLeaf(string.charAt(i));
+            abstractTextComponent.add(symbolLeaf);
+        }
+    }
 }
